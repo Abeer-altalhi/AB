4b4f046a5b0f7cffcdd8312061aa47b40af78ea7
Abeer Altalhi <abeerabdrabu17@gmail.com>
2022-10-29 15:48:07 +0300

    Initial Commit

---
+++ AB/ABApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABApp.swift
+//  AB
+//
+//  Created by Abeer on 04/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ABApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
